0120b9b51687e222301fa626117bb8770dd38c76
Ayush tomar <ayushtomar388@gmail.com>
2024-12-20 22:03:48 +0530

    Lab Sidebar component added

---
+++ src/components/LabSidebar.tsx
@@ -0,0 +1,75 @@
+const LabSidebar = () => {
+    return ( 
+        <div className="
+                hidden 
+                lg:block
+                min-w-60 sticky top-20 mt-6 pl-3 h-[85vh] overflow-y-scroll
+                [&::-webkit-scrollbar]:w-2
+                [&::-webkit-scrollbar-track]:bg-white
+                [&::-webkit-scrollbar-track]:rounded-full
+                [&::-webkit-scrollbar-thumb]:bg-[#4f576c50] 
+                [&::-webkit-scrollbar-thumb]:rounded-full
+        ">
+            <h2 className="font-semibold">UI Components</h2>
+            <ul className="mt-2 space-y-1 text-[#4f576c]">
+                <li>Navbar</li>
+                <li>Navbar</li>
+                <li>Navbar</li>
+                <li>Navbar</li>
+                <li>Navbar</li>
+                <li>Navbar</li>
+                <li>Navbar</li>
+                <li>Navbar</li>
+                <li>Navbar</li>
+                <li>Navbar</li>
+                <li>Navbar</li>
+                <li>Navbar</li>
+                <li>Navbar</li>
+                <li>Navbar</li>
+                <li>Navbar</li>
+                <li>Navbar</li>
+                <li>Navbar</li>
+                <li>Navbar</li>
+                <li>Navbar</li>
+                <li>Navbar</li>
+                <li>Navbar</li>
+                <li>Navbar</li>
+                <li>Navbar</li>
+                <li>Navbar</li>
+                <li>Navbar</li>
+                <li>Navbar</li>
+                <li>Navbar</li>
+                <li>Navbar</li>
+                <li>Navbar</li>
+                <li>Navbar</li>
+                <li>Navbar</li>
+                <li>Navbar</li>
+                <li>Navbar</li>
+                <li>Navbar</li>
+                <li>Navbar</li>
+                <li>Navbar</li>
+                <li>Navbar</li>
+                <li>Navbar</li>
+                <li>Navbar</li>
+                <li>Navbar</li>
+                <li>Navbar</li>
+                <li>Navbar</li>
+                <li>Navbar</li>
+                <li>Navbar</li>
+                <li>Navbar</li>
+                <li>Navbar</li>
+                <li>Navbar</li>
+                <li>Navbar</li>
+                <li>Navbar</li>
+                <li>Navbar</li>
+                <li>Navbar</li>
+                <li>Navbar</li>
+                <li>Navbar</li>
+                <li>Navbar</li>
+                <li>Navbar</li>
+            </ul>
+        </div>
+     );
+}
+ 
+export default LabSidebar;
